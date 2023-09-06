1028be91f32c90fd87c2464b1163943371893a8c
Jean Robles <jean.deyjean@gmail.com>
2023-09-06 15:12:07 -0500

    agregando las vistas para el envio de correo y para resetear

---
+++ SistemaVenta.AplicacionWeb/Controllers/PlantillaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class PlantillaController : Controller
+    {
+            /*
+            private readonly IMapper _mapper;
+            private readonly INegocioService _negocioService;
+            private readonly IVentaService _ventaService;
+            public PlantillaController(IMapper mapper, INegocioService negocioService, IVentaService ventaService)
+            {
+                _mapper = mapper;
+                _negocioService = negocioService;
+                _ventaService = ventaService;
+            }
+            */
+            public IActionResult EnviarClave(string correo, string clave)
+            {
+                ViewData["Correo"] = correo;
+                ViewData["Clave"] = clave;
+                ViewData["Url"] = $"{this.Request.Scheme}://{this.Request.Host}";
+
+                return View();
+            }
+            /*
+            public async Task<IActionResult> PDFVenta(string numeroVenta)
+            {
+                VMVenta vmVenta = _mapper.Map<VMVenta>(await _ventaService.Detalle(numeroVenta));
+
+                VMNegocio vmNegocio = _mapper.Map<VMNegocio>(await _negocioService.Obtener());
+
+                VMPDFVenta modelo = new VMPDFVenta();
+                modelo.negocio = vmNegocio;
+                modelo.venta = vmVenta;
+
+                return View(modelo);
+            }
+            */
+            public IActionResult RestablecerClave(string clave)
+            {
+                ViewData["Clave"] = clave;
+                return View();
+            }
+     }
+ 
+}

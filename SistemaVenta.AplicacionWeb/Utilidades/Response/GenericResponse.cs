4c0a91ec806a47db1754ced7eb39d6d481b4123e
Jean Robles <jean.deyjean@gmail.com>
2023-09-06 15:20:01 -0500

    carpeta utilidades para las respuestas a todas las solicitudes

---
+++ SistemaVenta.AplicacionWeb/Utilidades/Response/GenericResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaVenta.AplicacionWeb.Utilidades.Response
+{
+    //esta clase lo vamos a utilizar como respuestas a todas las solicitudes que se hagan a nuestro sitio web
+    public class GenericResponse<TObject>
+    {
+        public bool Estado { get; set; }
+        public string? Mensaje { get; set; }
+        public TObject? Objeto { get; set; }
+        public List<TObject>? ListaObjeto { get; set; }
+
+    }
+}

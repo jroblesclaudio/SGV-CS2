9823b76cc5d5f383c7d8761779a63083569bfa9b
Jean Robles <jean.deyjean@gmail.com>
2023-09-27 18:44:52 -0500

    creacion del servicio para categorias y implementacion de las solicitudes http para el modulo de categorias

--- SistemaVenta.AplicacionWeb/Controllers/CategoriaController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/CategoriaController.cs
@@ -1,12 +1,100 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class CategoriaController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriaController(IMapper mapper, ICategoriaService categoriaService)
+        {
+            _mapper = mapper;
+            _categoriaService = categoriaService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<VMCategoria> vmCategoriaLista = _mapper.Map<List<VMCategoria>>(await _categoriaService.Lista());
+
+            return StatusCode(StatusCodes.Status200OK, new { data = vmCategoriaLista });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] VMCategoria modelo)
+        {
+            GenericResponse<VMCategoria> gResponse = new GenericResponse<VMCategoria>();
+
+            try
+            {
+                Categoria categoria_creada = await _categoriaService.Crear(_mapper.Map<Categoria>(modelo));
+
+                modelo = _mapper.Map<VMCategoria>(categoria_creada);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = modelo;
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = true;
+                gResponse.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromBody] VMCategoria modelo)
+        {
+            GenericResponse<VMCategoria> gResponse = new GenericResponse<VMCategoria>();
+
+            try
+            {
+                Categoria categoria_editada = await _categoriaService.Editar(_mapper.Map<Categoria>(modelo));
+
+                modelo = _mapper.Map<VMCategoria>(categoria_editada);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = modelo;
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = true;
+                gResponse.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int idCategoria)
+        {
+            GenericResponse<string> gResponse = new GenericResponse<string>();
+
+            try
+            {
+                gResponse.Estado = await _categoriaService.Eliminar(idCategoria);
+
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, gResponse);
         }
     }
 }
--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -38,6 +38,8 @@ namespace SistemaVenta.IOC
 
             services.AddScoped<IUsuarioService, UsuarioService>();
             services.AddScoped<INegocioService, NegocioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+
 
 
         }

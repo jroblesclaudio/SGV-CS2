67aba8915d54fe110b006f47030dc9a3c117bb0b
Jean Robles <jean.deyjean@gmail.com>
2023-09-29 22:38:58 -0500

    solucion del error select2, todo por un ; en las dependencias jeje :)

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -41,7 +41,7 @@ namespace SistemaVenta.IOC
             services.AddScoped<ICategoriaService, CategoriaService>();
             services.AddScoped<IProductoService, ProductoService>();
 
-            services.AddScoped<ITipoDocumentoVentaService, TipoDocumentoVentaService>()
+            services.AddScoped<ITipoDocumentoVentaService, TipoDocumentoVentaService>();
             services.AddScoped<IVentaService, VentaService>();
 
 
